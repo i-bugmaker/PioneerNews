190730ee504dea2b2ce61095558a1510a2cdc3b9
menghui.huang <291147521@qq.com>
2026-04-14 17:03:53 +0800

    fix: 修复 SysVinit heredoc 语法错误

--- deploy.sh
+++ deploy.sh
@@ -706,21 +706,18 @@ create_sysvinit_script() {
     local venv_python="$VENV_DIR/bin/python"
     local main_py="$DEPLOY_DIR/main.py"
     
-    cat > "$init_script" << 'INITSCRIPT'
+    cat > "$init_script" << EOF
 #!/bin/bash
 ### BEGIN INIT INFO
 # Provides:          pioneernews
-# Required-Start:    $network $remote_fs
-# Required-Stop:     $network $remote_fs
+# Required-Start:    \$network \$remote_fs
+# Required-Stop:     \$network \$remote_fs
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: PioneerNews 财经新闻实时播报系统
 ### END INIT INFO
 
 NAME=pioneernews
-INITSCRIPT'
-    
-    cat >> "$init_script" << EOF
 DAEMON=${venv_python}
 DAEMON_ARGS=${main_py}
 PIDFILE=${PID_FILE}
